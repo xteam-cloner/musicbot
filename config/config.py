a26751e6efe6b35027d47003637e955455e8ce45
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-29 16:22:10 +0700

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -1,15 +1,15 @@
 import os
 
 
-API_ID: int = int(os.getenv("API_ID", 0))
+API_ID: int = int(os.getenv("API_ID", "22049281"))
 
-API_HASH: str = os.getenv("API_HASH", "")
+API_HASH: str = os.getenv("API_HASH", "0377071c177fea820b5086cb8298ed64")
 
 SESSION_STRING: str = os.getenv("SESSION_STRING", "")
 
-OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", 0))]
+OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", "1434595544"))]
 
-LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", 0))
+LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", "-1002385138723"))
 
 PREFIX: str = str(".")
 
